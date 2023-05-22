57a8c54ea29bd18ce686a6fe7a9554d617eef1dc
ysparrk <youngseo0703@gmail.com>
2023-05-22 16:59:40 +0900

    accounts: my_page

---
+++ front-server/src/components/Accounts/MyPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>my page입니다</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyPage',
+}
+</script>
+
+<style>
+
+</style>
--- front-server/src/views/MovieView.vue
+++ front-server/src/views/MovieView.vue
@@ -3,18 +3,15 @@
   <p>{{ username }} 입니다</p>
   <h1>오늘 영화 어때요?</h1>
   <OttListButton />
-  <!-- <MovieList/> -->
   </div>
 </template>
 
 <script>
-// import MovieList from '@/components/MovieList/MovieList.vue'
 import OttListButton from '@/components/MovieList/OttListButton.vue'
 
 export default {
   name: 'MovieView',
   components: {
-    // MovieList,
     OttListButton,
   },
   computed: {
