a979ef680c99f2819950f8ebe69807805efb1e60
heejinssss <heejin.dev33@gmail.com>
2023-05-24 22:46:24 +0900

    css detail

--- front-server/src/components/MovieList/MovieDetail.vue
+++ front-server/src/components/MovieList/MovieDetail.vue
@@ -1,12 +1,29 @@
 <template>
   <div>
-
     <div class="backdropcontainer"
-    :style="{ backgroundImage: `linear-gradient( rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.9) ), url(https://image.tmdb.org/t/p/original${movie.backdrop_path})`}">
+    :style="{ backgroundImage: `linear-gradient( rgba(255, 255, 255, 0.4), rgba(0, 0, 0, 0.8) ), url(https://image.tmdb.org/t/p/original${movie.backdrop_path})`}">
 
 
-      <div v-if="movie" class="inner">
+      <div v-if="movie">
         <p class="title">{{ movie.title }}</p>
+
+
+        <div class="card" style="width:700px; margin:auto;">
+          <div v-if="movie.video_key" class="card-img-top">
+            <iframe id="player" width="700px" height="360px"
+            :src="`https://www.youtube.com/embed/${movie.video_key}`"
+            frameborder="0">
+            </iframe>
+          </div>
+
+          <div class="card-img-top" v-else>
+            <img :src="'https://image.tmdb.org/t/p/original'+ movie.backdrop_path" style="width:640px; height:auto;">
+          </div>
+
+          <div class="card-body">
+            <div class="card-text">{{ movie.overview }}</div>
+          </div>
+        </div>
 
         <div v-if="ott_lst" class="ott-btn" style="margin-bottom:20px;">
           <span v-for="(ott, idx) in ott_lst" :key="idx" style="margin:5px;">
@@ -14,26 +31,15 @@
           </span>
         </div>
 
+        <!-- <button @click="userLikes(movie.id)">
+          {{ likes ? '좋아요' : '좋아요  취소' }}
+        </button> -->
 
-        <div class="card">
-        <div v-if="movie.video_key" class="card-img-top">
-          <iframe id="player" width="640" height="360"
-          :src="`https://www.youtube.com/embed/${movie.video_key}`"
-          frameborder="0">
-          </iframe>
-        </div>
-
-        <div class="card-img-top" v-else>
-          <img :src="'https://image.tmdb.org/t/p/original'+ movie.backdrop_path" style="width:640px; height:auto;">
-        </div>
-
-        <div class="card-body">
-          <p class="card-text">{{ movie.overview }}</p>
-        </div>
-        </div>
-
-
-        <button @click="userLikes(movie.id)">{{ likes ? '좋아요' : '좋아요  취소' }}</button>
+        <button type="button" class="like btn btnEvent" @click="userLikes(movie.id)">
+          <img v-if="likes" :src="require('@/assets/heart_after.png')" style="width: 35px; height: 35px;">
+          <img v-else :src="require('@/assets/heart_before.png')" style="width: 35px; height: 35px;">
+        </button>
+        
         <p>{{ movie.likes_count }}</p>
         <MovieCommentCreate :movie="movie" v-if="movie"/>
       </div>
@@ -154,15 +160,21 @@ export default {
 </script>
 
 <style scoped>
+.backdropcontainer {
+  background-repeat: no-repeat;
+  background-position: center top;
+}
 @font-face {
   font-family: 'MBC1961M';
   src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2304-01@1.0/MBC1961M.woff2') format('woff2');
   font-weight: normal;
   font-style: normal;
 }
-.backdropcontainer {
-  background-repeat: no-repeat;
-  background-position: center top;
+@font-face {
+  font-family: 'LINESeedKR-Bd';
+  src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_11-01@1.0/LINESeedKR-Bd.woff2') format('woff2');
+  font-weight: 700;
+  font-style: normal;
 }
 p.title {
   padding-top: 20px;
@@ -174,8 +186,73 @@ p.title {
   border-radius: 10px;
 }
 .card {
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  background: transparent;
 }
+@font-face {
+  font-family: 'Arita-buri-SemiBold';
+  src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_one@1.0/Arita-buri-SemiBold.woff') format('woff');
+  font-weight: normal;
+  font-style: normal;
+}
+@font-face {
+  font-family: 'ChosunCentennial';
+  src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2206-02@1.0/ChosunCentennial.woff2') format('woff2');
+  font-weight: normal;
+  font-style: normal;
+}
+/* @font-face {
+  font-family: 'Dokrip';
+  src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_twelve@1.1/Dokrip.woff') format('woff');
+  font-weight: normal;
+  font-style: normal;
+} */
+.card {
+  border: transparent;
+}
+.card-text {
+  color: white;
+  font-size: 20px;
+  font-family: ChosunCentennial;
+}
+.like:focus{ 	
+  border: none;
+  outline: none;
+}
+.like:hover {
+  transition: 0.2s ease-out;
+  opacity : 0.8;
+}
+.like:not(:hover) {
+  transition: 0.2s ease-out;
+}
+/* @keyframes jello-horizontal {
+0% {
+  -webkit-transform: scale3d(1, 1, 1);
+          transform: scale3d(1, 1, 1);
+}
+30% {
+  -webkit-transform: scale3d(1.25, 0.75, 1);
+          transform: scale3d(1.25, 0.75, 1);
+}
+40% {
+  -webkit-transform: scale3d(0.75, 1.25, 1);
+          transform: scale3d(0.75, 1.25, 1);
+}
+50% {
+  -webkit-transform: scale3d(1.15, 0.85, 1);
+          transform: scale3d(1.15, 0.85, 1);
+}
+65% {
+  -webkit-transform: scale3d(0.95, 1.05, 1);
+          transform: scale3d(0.95, 1.05, 1);
+}
+75% {
+  -webkit-transform: scale3d(1.05, 0.95, 1);
+          transform: scale3d(1.05, 0.95, 1);
+}
+100% {
+  -webkit-transform: scale3d(1, 1, 1);
+          transform: scale3d(1, 1, 1);
+}
+} */
 </style>
